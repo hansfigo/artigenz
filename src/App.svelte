f7042400d9a6a533cfad8a3969fe8ca4ab3465f2
HansFigo <hambasahaya0303@gmail.com>
2024-06-14 13:22:24 +0700

    Responsive

--- src/App.svelte
+++ src/App.svelte
@@ -67,72 +67,71 @@
 
     <!--Main-->
     <div
-      class="container pt-24 md:pt-36 mx-auto flex flex-wrap flex-col  items-center h-full"
+      class="container pt-24 md:pt-36 mx-auto flex flex-wrap flex-col items-center h-full"
     >
       <!--Left Col-->
-      <div class="flex justify-between w-full">
-
-      <div
-        class="flex flex-col w-full  justify-center lg:items-start overflow-y-hidden"
-      >
-        <h1
-          class="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left"
+      <div class="flex justify-between flex-col md:flex-row w-full">
+        <div
+          class="flex flex-col w-full justify-center lg:items-start overflow-y-hidden"
         >
-          <span
-            class="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500"
+          <h1
+            class="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left"
           >
-            Next gen
-          </span>
-          3D Platform to Brand yourself!
-        </h1>
-        <p
-          class="leading-normal text-base md:text-2xl mb-8 text-center md:text-left"
-        >
-          Brand yourself with our next-gen 3D platform now !
-        </p>
-
-        <form
-          class="bg-gray-900 opacity-75 w-full shadow-lg rounded-lg px-8 pt-6 pb-8 mb-4"
-        >
-          <div class="mb-4">
-            <label
-              class="block text-blue-300 py-2 font-bold mb-2"
-              for="emailaddress"
+            <span
+              class="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500"
             >
-              Signup for our newsletter
-            </label>
-            <input
-              class="shadow appearance-none border rounded w-full p-3 text-gray-700 leading-tight focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
-              id="emailaddress"
-              type="text"
-              placeholder="you@somewhere.com"
-            />
-          </div>
+              Next gen
+            </span>
+            3D Platform to Brand yourself!
+          </h1>
+          <p
+            class="leading-normal text-base md:text-2xl mb-8 text-center md:text-left"
+          >
+            Brand yourself with our next-gen 3D platform now !
+          </p>
 
-          <div class="flex items-center justify-between pt-4">
-            <button
-              class="bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
-              type="button"
-            >
-              Sign Up
-            </button>
-          </div>
-        </form>
-      </div>
+          <form
+            class="bg-gray-900 opacity-75 w-full shadow-lg rounded-lg px-8 pt-6 pb-8 mb-4"
+          >
+            <div class="mb-4">
+              <label
+                class="block text-blue-300 py-2 font-bold mb-2"
+                for="emailaddress"
+              >
+                Signup for our newsletter
+              </label>
+              <input
+                class="shadow appearance-none border rounded w-full p-3 text-gray-700 leading-tight focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+                id="emailaddress"
+                type="text"
+                placeholder="you@somewhere.com"
+              />
+            </div>
 
-      <!--Right Col-->
-      <div class=" w-full  relative  flex h-[42rem]">
-        <div class="p-12 overflow-hidden "></div>
-        <!-- <img
+            <div class="flex items-center justify-between pt-4">
+              <button
+                class="bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+                type="button"
+              >
+                Sign Up
+              </button>
+            </div>
+          </form>
+        </div>
+
+        <!--Right Col-->
+        <div class=" w-full hidden md:block md:flex relative h-[42rem]">
+          <div class="p-12 overflow-hidden"></div>
+          <!-- <img
           class="mx-auto w-full md:w-4/5 transform -rotate-6 transition hover:scale-105 duration-700 ease-in-out hover:rotate-6"
           src={macbook}
           alt="macbook"
         /> -->
-        <div class=" w-full absolute">
-          <Canvas />
+          <div class=" w-full absolute">
+            <Canvas />
+          </div>
         </div>
       </div>
-    </div>
 
       <div class="mx-auto md:pt-16">
         <p class="text-blue-400 font-bold pb-8 lg:pb-6 text-center">
